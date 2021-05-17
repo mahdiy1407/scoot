16de83dbf3fdf70c1f9e5c9f40a57b0786dc2a70
UnPotato1407 <mahdiysulaymonov66@gmail.com>
2021-05-17 07:47:32 -0700

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,6 @@
+var path = document.querySelector(".faq-button")
+var description = document.querySelector(".faq__question-description")
+path.addEventListener("click", function () {
+    path.classList.toggle("faq-button--active")
+    description.classList.toggle("faq__question-description--active")
+})
