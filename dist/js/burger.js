eb1e456d6731569b2b8543c1c7dccc1cc03fb75f
UnPotato1407 <mahdiysulaymonov66@gmail.com>
2021-05-18 08:14:21 -0700

    mobile changes

---
+++ dist/js/burger.js
@@ -0,0 +1,5 @@
+var burger = document.querySelector(".hamburger");
+
+burger.addEventListener("click", function () {
+    burger.classList.toggle("hamburger--active");
+});
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
-var path = document.querySelector(".faq-button")
-var description = document.querySelector(".faq__question-description")
+var path = document.querySelector(".faq-button");
+var description = document.querySelector(".faq__question-description");
+
 path.addEventListener("click", function () {
-    path.classList.toggle("faq-button--active")
-    description.classList.toggle("faq__question-description--active")
-})
+    path.classList.toggle("faq-button--active");
+    description.classList.toggle("faq__question-description--active");
+});
